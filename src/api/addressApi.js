b3c31fe2e0b8fa7fa5a1761caa7848394e735e57
anyong <andrew4may@gmail.com>
2025-01-30 10:24:32 +0900

    Feat: checkout and address page

---
+++ src/api/addressApi.js
@@ -0,0 +1,96 @@
+import api from '@/api';
+
+export default {
+  // addressList 불러오기
+  async getAddressList(userNo) {
+    try {
+      const { data } = await api.get(
+        `/address/list/${userNo}`
+      );
+      console.log('address list :', data);
+      return data;
+    } catch (error) {
+      console.error(
+        'Failed to load data',
+        error.response.data
+      );
+    }
+  },
+  // 특정 address 불러오기
+  async getAddress(addrNo) {
+    try {
+      const { data } = await api.get(`/address/${addrNo}`);
+      console.log('address:', data);
+      return data;
+    } catch (error) {
+      console.error(
+        'Failed to load data',
+        error.response.data
+      );
+    }
+  },
+  // address default 불러오기
+  async getAddressDefault(userNo) {
+    try {
+      const { data } = await api.get(
+        `/address/default/${userNo}`
+      );
+      console.log('default address :', data);
+      return data;
+    } catch (error) {
+      console.error(
+        'Failed to load data',
+        error.response.data
+      );
+    }
+  },
+  // address 추가
+  async addAddress(userNo, address) {
+    try {
+      const { data } = await api.post(
+        `/address/add/${userNo}`,
+        address
+      );
+      console.log('address added :', data);
+      return data;
+    } catch (error) {
+      console.error(
+        'Failed to add address:',
+        error.response?.data || error.message
+      );
+      throw error; // 에러를 던져 상위에서 처리
+    }
+  },
+  // address update
+  async updateAddress(addrNo, address) {
+    try {
+      const { data } = await api.put(
+        `/address/update/${addrNo}`,
+        address
+      );
+      console.log('address added :', data);
+      return data;
+    } catch (error) {
+      console.error(
+        'Failed to add address:',
+        error.response?.data || error.message
+      );
+      throw error; // 에러를 던져 상위에서 처리
+    }
+  },
+  // address 삭제
+  async deleteAddress(addrNo) {
+    try {
+      const { data } = await api.delete(
+        `/address/delete/${addrNo}`
+      );
+      console.log('default address :', data);
+      return data;
+    } catch (error) {
+      console.error(
+        'Failed to delete data',
+        error.response.data
+      );
+    }
+  },
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/pages/Home.vue';
+import Checkout from '@/pages/Checkout.vue';
+import Address from '@/pages/Address.vue';
+import AddAddress from '@/pages/AddAddress.vue';
+import UpdateAddress from '@/pages/UpdateAddress.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/checkout',
+      name: 'Checkout',
+      component: Checkout,
+    },
+    {
+      path: '/address/:userNo',
+      name: 'Address',
+      component: Address,
+    },
+    {
+      path: '/addaddress',
+      name: 'AddAddress',
+      component: AddAddress,
+    },
+    {
+      path: '/updateaddress/:addrNo',
+      name: 'UpdateAddress',
+      component: UpdateAddress,
     },
   ],
 });
