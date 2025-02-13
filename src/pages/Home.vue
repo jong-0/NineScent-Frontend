9960dd9da7f4b08368f742f7d267087dcb2eea3c
ruedal <gpfls6008@naver.com>
2025-02-13 20:18:33 +0900

    Feat: ItemDetail.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,7 +10,12 @@
       <section class="featured-products">
         <h2>Featured Products</h2>
         <div class="product-list">
-          <div v-for="product in featuredProducts" :key="product.id" class="product-item">
+          <div
+            v-for="product in featuredProducts"
+            :key="product.id"
+            class="product-item"
+            @click="goToDetail(product.id)"
+          >
             <img :src="product.image" :alt="product.name" />
             <h3>{{ product.name }}</h3>
             <p>{{ product.price }}</p>
@@ -23,27 +28,37 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
+
+const router = useRouter();
+
+const goToDetail = (productId) => {
+  router.push(`/items/${productId}`);
+};
 
 const featuredProducts = [
   {
     id: 1,
-    name: 'Rose Elegance',
-    price: '$89',
-    image: 'https://via.placeholder.com/150',
+    name: '딥 다운 핸드 밤 50ml',
+    price: '32,000원',
+    image:
+      'https://items-img-bucket.s3.ap-northeast-2.amazonaws.com/main/%E1%84%83%E1%85%B5%E1%86%B8+%E1%84%83%E1%85%A1%E1%84%8B%E1%85%AE%E1%86%AB+%E1%84%92%E1%85%A2%E1%86%AB%E1%84%83%E1%85%B3+%E1%84%87%E1%85%A1%E1%86%B7+50ml.jpg',
   },
   {
     id: 2,
     name: 'Citrus Bloom',
     price: '$79',
-    image: 'https://via.placeholder.com/150',
+    image:
+      'https://items-img-bucket.s3.ap-northeast-2.amazonaws.com/main/2.jpg',
   },
   {
     id: 3,
     name: 'Mystic Oud',
     price: '$99',
-    image: 'https://via.placeholder.com/150',
+    image:
+      'https://items-img-bucket.s3.ap-northeast-2.amazonaws.com/main/3.jpg',
   },
 ];
 </script>
@@ -81,12 +96,20 @@ const featuredProducts = [
   gap: 20px;
   justify-content: center;
 }
-.product-item {
-  text-align: center;
-}
+
 .product-item img {
   width: 150px;
   height: 150px;
   object-fit: cover;
+}
+
+.product-item {
+  text-align: center;
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+
+.product-item:hover {
+  transform: scale(1.05);
 }
 </style>
