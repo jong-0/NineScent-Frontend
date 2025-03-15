e11f3730015eef4303c17ca18af5fcb0bb55f300
CoderAnnyeong <andrew4may@gmail.com>
2025-03-15 19:41:46 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: '54.180.99.176:8080',
                 // changeOrigin: true,
             },
         },
